474fe1a7dce213f0563bb7e1616bc0a40d5ddd7f
AY <sashayenza1996@gmail.com>
2020-09-24 22:55:29 -0400

    switch page #1
    Плюсы такого подхода:
    
    тест выглядит аккуратнее — не нужно инициализировать страницу в теле теста
    явно возвращаем страницу — тип страницы ассоциирован с методом
    не нужно каждый раз думать в разных тестах про инициализацию страницы — уменьшаем дублирование кода
    минусы:
    
    если у нас копится большое количество страниц и переходов — образуется много перекрестных импортов
    большая связность кода — при изменении логики придется менять возвращаемое значение
    сложнее понимать код, так как страница инициализируется неявно
    образуются циклические зависимости, что часто приводит к ошибкам

--- test/test_main_page.py
+++ test/test_main_page.py
@@ -7,8 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    page.should_be_link_login()
+    login_page = page.go_to_login_page() #1
+    login_page.should_be_link_login() #1
 
 
 def test_login_page_elements_present(browser):
