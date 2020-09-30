770445c0b1597076cb9dc1a8038f1a40c9e0500c
AY <sashayenza1996@gmail.com>
2020-09-29 21:28:14 -0400

    элемента нет элемент пропадает
    нужно дописать guest_ :
    
    test_guest_cant_see_success_message_after_adding_product_to_basket:
    Открываем страницу товара
    Добавляем товар в корзину
    Проверяем, что нет сообщения об успехе с помощью is_not_element_present
    
    test_guest_cant_see_success_message:
    Открываем страницу товара
    Проверяем, что нет сообщения об успехе с помощью is_not_element_present
    
    test_message_disappeared_after_adding_product_to_basket:
    Открываем страницу товара
    Добавляем товар в корзину
    Проверяем, что нет сообщения об успехе с помощью is_disappeared

--- test/page/base_page.py
+++ test/page/base_page.py
@@ -1,7 +1,7 @@
 import math
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException  # в начале файла
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class BasePage():
@@ -11,15 +11,9 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+
     def open(self):
         self.browser.get(self.url)
-
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except (NoSuchElementException):
-            return False
-        return True
 
 
     def solve_quiz_and_get_code(self):
@@ -36,6 +30,27 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
-    def tuple_to_str(self, tup):
-        str = ''.join(tup)
-        return str
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
